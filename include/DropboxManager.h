976c8d59ffe640a070fc2d032f020e7956d1e979
niksonber <nik.son18@hotmail.com>
2021-05-20 23:08:23 -0300

    Adding comments

--- include/DropboxManager.h
+++ include/DropboxManager.h
@@ -7,16 +7,32 @@
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 
+/// Class to download and upload files in Dropbox
 class DropboxManager{
 public:
+    /// Constructor @param token (optional)
     DropboxManager(const char * token = NULL);
+    
+    /// initiliza SPIFFS and set token @param token 
     void begin(const char * token = NULL);
+
+    /// Upload file in Dropbox @param filename filename in SPIFFS @param dropbox_path (optinal) name/path in Dropbox.
+    /// if not provided, filename is used @return true if sucessfull upload false otherwise.
     bool upload(const char * filename, const char * path = nullptr);
+    
+    /// Download file from Dropbox @param dropbox_path name/path in Dropbox. @param filename (optinal) filename in SPIFFS
+    /// if not provided, dropbox_path is used @return true if sucessfull download false otherwise.
     bool download(const char * path, const char * filename = nullptr, bool replace = false);
+
 protected:
     String _token;
+    
+    /// Dropbox api endpoint 
     const char * _endpoint = "https://content.dropboxapi.com/2/files/";
 
+    /// Upload or download file to/from Dropbox, mathod used internaly
+    /// @param dropbox_path (optinal) name/path in Dropbox. @param file_pointer pointer to opened file 
+    //  s@return true if sucessfull upload/download false otherwise.
     bool uploadOrDownload(String path, File * file, bool upload = true);
 };
 
